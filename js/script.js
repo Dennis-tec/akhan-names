6aef055a689f5027d468b5f88faf2e53df91c9c3
Dennis Yiaile <ykamakei6345@gmail.com>
2020-09-12 14:02:12 +0300

    add css background styles to the document

--- js/script.js
+++ js/script.js
@@ -17,31 +17,43 @@ function akhan(){
   birthMonth=parseInt(birthDate).slice(5, 7);
   birthDay=parseInt(birthDate).slice(8, 10);
   year=parseInt(birthDate.slice(0, 4);
-  if ((birthDay<= || birthDay>31) || (birthMonth<=0 || birthMonth>12)){
+  if ((birthDay<= 0 || birthDay > 31) || (birthMonth<= 0 || birthMonth> 12)){
     alert("You entered an invalid date");
   }
-  var weekDay;
+  var weekDays;
   var maleNames;
   var femaleNames;
-  weekDay=["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+  weekDays=["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
   maleNames=["Kwasi", "Kwadwo", "Kwabena", "Kwaku","Yaw", "Kofi", "Kwame"];
   femaleNames=["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
   var dayOfTheWeek;
   var day;
-  dayOfTheWeek=( ( (centuryYear/4) -2*centuryYear-1) + ((5*birthYear/4) ) + ((26*(birthMonth+1)/10)) + birthDay ) % 7;
-  if (dayOfTheWeek<0) {
-    day=Math.round(dayOfTheWeek)+7;
+  dayOfTheWeek =( ( (centuryYear/4) -2*centuryYear-1) + ((5*birthYear/4) ) + ((26*(birthMonth+1)/10)) + birthDay ) % 7;
+  if (dayOfTheWeek< 0) {
+    day = Math.round(dayOfTheWeek)+7;
   }else {
-    day=Math.trunc(dayOfTheWeek);
+    day = Math.trunc(dayOfTheWeek);
   }
-  var akanName;
+  if (year < 1920 || (year > 1920 && year < 1979)){
+day=day -1;
+  }
+  if (year >= 1980 && year < 2000) {
+    day -= 1;
+  }
+  if (year === 1979) {
+    day = day +6;
+  }
+  if (year === 1990) {
+    day = day+ 7
+  }
+  var akhanName;
   var dayName;
-  if (gender==="male") {
-    akanName=maleNames[day]
-    dayName=weekDay[day];
+  if (gender === "male") {
+    akanName = maleNames[day];
+    dayName = weekDay[day];
   }else {
-    akhanName=femaleNames[day];
-    dayName=weekDay[day];
+    akhanName = femaleNames[day];
+    dayName = weekDay[day];
   }
-  document.getElementById('output').innerHTML='Your akhan name is: ' + akanName +'meaning you were born on' +dayName +'.'
+  document.getElementById('output').innerHTML='Your akhan name is: ' + akhanName +'meaning you were born on' +dayName;
 }
